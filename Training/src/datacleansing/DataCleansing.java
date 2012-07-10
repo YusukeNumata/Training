049dc93ed312918f6651349188deff71c245a710
YusukeNumata <YusukeNumata@gmail.com>
2012-07-10 23:14:29 +0900

    データクレンジング用のクラスを作成してみた。 単純にStringのreplaceメソッドだとやっぱり遅いので、ちょっと変えた方法を採用してみた。

---
+++ Training/src/datacleansing/DataCleansing.java
@@ -0,0 +1,59 @@
+package datacleansing;
+
+import java.util.HashMap;
+
+public class DataCleansing {
+
+	/**
+	 * 文字列を整形する.
+	 * 置換毎にStringが生成されるので、遅い.
+	 * @param param
+	 * @return
+	 */
+	public static String execute(String param) {
+		param = param.replace("ァ", "ア");
+		param = param.replace("ィ", "イ");
+		param = param.replace("ゥ", "ウ");
+		param = param.replace("ェ", "エ");
+		param = param.replace("ォ", "オ");
+		param = param.replace("ｱ", "ア");
+		param = param.replace("ｲ", "イ");
+		param = param.replace("ｳ", "ウ");
+		param = param.replace("ｴ", "エ");
+		param = param.replace("ｵ", "オ");
+		return param;
+	}
+
+	private static HashMap<Character, Character> replaceMap;
+
+	static {
+		replaceMap = new HashMap<Character, Character>();
+		replaceMap.put('ァ', 'ア');
+		replaceMap.put('ィ', 'イ');
+		replaceMap.put('ゥ', 'ウ');
+		replaceMap.put('ェ', 'エ');
+		replaceMap.put('ォ', 'オ');
+		replaceMap.put('ｱ', 'ア');
+		replaceMap.put('ｲ', 'イ');
+		replaceMap.put('ｳ', 'ウ');
+		replaceMap.put('ｴ', 'エ');
+		replaceMap.put('ｵ', 'オ');
+	}
+
+	/**
+	 * 文字列を整形する.
+	 * いったん、char[]に変換して動作するのである程度早いはず！
+	 * @param param
+	 * @return
+	 */
+	public static String executeFast(String param) {
+		char[] chars = param.toCharArray();
+		for (int i = 0; i < chars.length; i++) {
+			Character ch = replaceMap.get(chars[i]);
+			if (ch != null) {
+				chars[i] = ch;
+			}
+		}
+		return new String(chars);
+	}
+}
---
+++ Training/test/datacleansing/DataCleansingTest.java
@@ -0,0 +1,49 @@
+package datacleansing;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class DataCleansingTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		assertEquals("ア", DataCleansing.execute("ァ"));
+		assertEquals("イ", DataCleansing.execute("ィ"));
+		assertEquals("アイウエオアイウエオ", DataCleansing.execute("ァィゥェォｱｲｳｴｵ"));
+	}
+
+	@Test
+	public void testExecute() {
+		for(int i = 0; i < 100000; i++) {
+			assertEquals("アイウエオアイウエオ", DataCleansing.execute("ァィゥェォｱｲｳｴｵ"));
+		}
+	}
+
+	@Test
+	public void testExecuteFast() {
+		for(int i = 0; i < 100000; i++) {
+			assertEquals("アイウエオアイウエオ", DataCleansing.executeFast("ァィゥェォｱｲｳｴｵ"));
+		}
+	}
+}
