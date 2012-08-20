71f3ad2dbb2907b54e8f79ed811440864575b449
YusukeNumata <YusukeNumata@gmail.com>
2012-08-21 00:14:07 +0900

    WEB+DB JUnit実践入門 乗算メソッドのテストまで

---
+++ Training/src/yusuke/numata/junit/tutorial/Calculator.java
@@ -0,0 +1,12 @@
+package yusuke.numata.junit.tutorial;
+
+public class Calculator {
+
+	public int multiply(int x, int y) {
+		return x * y;
+	}
+	
+	public int divide(int x, int y) {
+		return x / y;
+	}
+}
---
+++ Training/test/yusuke/numata/junit/tutorial/CalculatorTest.java
@@ -0,0 +1,51 @@
+package yusuke.numata.junit.tutorial;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class CalculatorTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void multiplyで乗算結果が取得できる() {
+		Calculator calc = new Calculator();
+		int expected = 12;
+		int actual = calc.multiply(3, 4);
+		assertThat(actual, is(expected));
+	}
+
+	@Test
+	public void multiplyで5と7の乗算結果が取得できる() {
+		Calculator calc = new Calculator();
+		int expected = 35;
+		int actual = calc.multiply(5, 7);
+		assertThat(actual, is(expected));
+	}
+	
+	@Test
+	public void testDivide() {
+		fail("Not yet implemented");
+	}
+
+}
