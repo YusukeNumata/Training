10cfa899feda040ace9ad24e6b5807d28e12be20
YusukeNumata <YusukeNumata@gmail.com>
2012-08-21 23:56:46 +0900

    WEB+DB JUnit実践入門 ルールとは？まで

--- Training/test/yusuke/numata/junit/tutorial/CalculatorTest.java
+++ Training/test/yusuke/numata/junit/tutorial/CalculatorTest.java
@@ -5,14 +5,18 @@ import static org.junit.Assert.*;
 
 import org.junit.After;
 import org.junit.AfterClass;
+import org.junit.Assume;
 import org.junit.Before;
 import org.junit.BeforeClass;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.experimental.runners.Enclosed;
 import org.junit.experimental.theories.DataPoint;
 import org.junit.experimental.theories.DataPoints;
 import org.junit.experimental.theories.Theories;
 import org.junit.experimental.theories.Theory;
+import org.junit.internal.runners.statements.ExpectException;
+import org.junit.rules.ExpectedException;
 import org.junit.runner.RunWith;
 
 @RunWith(Enclosed.class)
@@ -50,19 +54,49 @@ public class CalculatorTest {
 		}
 	}
 	
-	
-	@Test
-	public void divideで3と2の除算結果が取得できる() {
-		Calculator calc = new Calculator();
-		float expected = 1.5f;
-		float actual = calc.divide(3, 2);
-		assertThat(actual, is(expected));
-	}
-	
-	@Test(expected=IllegalArgumentException.class)
-	public void divideの第２引数に0を指定した場合はIllegalArgumentExceptionを送出する() {
-		Calculator calc = new Calculator();
-		calc.divide(3, 0);
+	@RunWith(Theories.class)
+	public static class 除算メソッドのパラメータ化テスト {
+		
+		@DataPoints
+		public static Fixture[] DATAS = {
+			new Fixture(3, 2, 1.5f),
+			new Fixture(10, 2, 5f),
+			new Fixture(3, 0, null),
+			new Fixture(10, 0, null)
+		};
+		
+		@Rule
+		public ExpectedException exception = ExpectedException.none();
+		
+		@Theory
+		public void divideで除算結果が取得できる(Fixture fx) {
+			Assume.assumeTrue(fx.y != 0);	// 0以外なら以降の処理を実行
+			System.out.println(fx.x + "/" + fx.y + "=" + fx.expected);
+			Calculator calc = new Calculator();
+			float expected = fx.expected;
+			float actual = calc.divide(fx.x, fx.y);
+			assertThat(actual, is(expected));
+		}
+
+		@Theory
+		public void divideの第２引数に0を指定した場合はIllegalArgumentExceptionを送出する(Fixture fx) {
+			Assume.assumeTrue(fx.y == 0);	// 0なら以降の処理を実行
+			System.out.println(fx.x + "/" + fx.y + "=" + fx.expected + " 0で割っちゃだめ？");
+			exception.expect(IllegalArgumentException.class);
+			Calculator calc = new Calculator();
+			calc.divide(fx.x, fx.y);
+		}
+
+		// データクラス
+		static class Fixture {
+			int x, y;
+			Float expected;
+			Fixture(int x, int y, Float expected) {
+				this.x = x;
+				this.y = y;
+				this.expected = expected;
+			}
+		}
 	}
 
 }
