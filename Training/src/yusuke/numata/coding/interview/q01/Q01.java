475f817282c5089798e5af7b37f6905b352089b0
YusukeNumata <YusukeNumata@gmail.com>
2012-12-12 00:05:16 +0900

    「世界で闘うプログラミング力を鍛える150問」の問題1-1の解答 本の解答をコーディング・テストした。 自分で作ったメソッドの単語が誤っていたので修正

--- Training/src/yusuke/numata/coding/interview/q01/Q01.java
+++ Training/src/yusuke/numata/coding/interview/q01/Q01.java
@@ -3,9 +3,18 @@ package yusuke.numata.coding.interview.q01;
 import java.util.HashSet;
 import java.util.Set;
 
+/**
+ * Chapter 1 配列と文字列 問題1.1
+ * @author yusuke
+ */
 public class Q01 {
 
-	public boolean isUnice(String str) {
+	/**
+	 * ある文字列がすべてユニークである(重複する文字がない)かどうか判定する.
+	 * @param str 文字列
+	 * @return true:すべてユニーク、false:重複文字あり
+	 */
+	public boolean isUnique(String str) {
 		char[] chr = str.toCharArray();
 		Set<Character> s = new HashSet<Character>();
 		for (char c : chr) {
@@ -15,6 +24,45 @@ public class Q01 {
 			} else {
 				s.add((Character) c);
 			}
+		}
+		return true;
+	}
+
+	/**
+	 * 本の解答１.
+	 * <p>ただし、アルファベットの小文字限定.
+	 * <p>ビットベクトルを用いて消費メモリを<code>isUniqueChars2</code>の8分の1に抑える
+	 * @param str 文字列(アルファベット小文字限定)
+	 * @return true:すべてユニーク、false:重複文字あり
+	 */
+	public boolean isUniqueChars(String str) {
+		if (str.length() > 256)
+			return false;
+		int checker = 0;
+		for (int i = 0; i < str.length(); i++) {
+			int val = str.charAt(i) - 'a';
+			if ((checker & (1 << val)) > 0)
+				return false;
+			checker |= (1 << val);
+		}
+		return true;
+	}
+
+	/**
+	 * 本の解答2.
+	 * <p>ただし、文字の種類が256しかない前提.
+	 * @param str 文字列
+	 * @return true:すべてユニーク、false:重複文字あり
+	 */
+	public boolean isUniqueChars2(String str) {
+		if (str.length() > 256)
+			return false;
+		boolean[] char_set = new boolean[256];
+		for (int i = 0; i < str.length(); i++) {
+			int val = str.charAt(i);
+			if (char_set[val])		// この文字がすでに文字列中で現れている
+				return false;
+			char_set[val] = true;
 		}
 		return true;
 	}
--- Training/test/yusuke/numata/coding/interview/q01/Q01Test.java
+++ Training/test/yusuke/numata/coding/interview/q01/Q01Test.java
@@ -1,48 +1,83 @@
 package yusuke.numata.coding.interview.q01;
 
 import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertThat;
 
 import org.junit.After;
-import org.junit.AfterClass;
 import org.junit.Before;
-import org.junit.BeforeClass;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TestName;
 
 public class Q01Test {
 
-	@BeforeClass
-	public static void setUpBeforeClass() throws Exception {
-	}
-
-	@AfterClass
-	public static void tearDownAfterClass() throws Exception {
-	}
+	@Rule
+	public TestName name = new TestName();
 
 	@Before
 	public void setUp() throws Exception {
+		System.out.println("start:" + Runtime.getRuntime().freeMemory() + " " + name.getMethodName());
 	}
 
 	@After
 	public void tearDown() throws Exception {
+		System.out.println("end  :" + Runtime.getRuntime().freeMemory() + " " + name.getMethodName());
 	}
 
 	@Test
-	public void testIsUnice() {
+	public void testIsUnique() {
 		Q01 q = new Q01();
-		assertThat(q.isUnice("a"),   is(true));
-		assertThat(q.isUnice("aa"),  is(false));
-		assertThat(q.isUnice("ab"),  is(true));
-		assertThat(q.isUnice("abc"), is(true));
-		assertThat(q.isUnice("abca"), is(false));
-		assertThat(q.isUnice("ああ"), is(false));
+		assertThat(q.isUnique("a"),   is(true));
+		assertThat(q.isUnique("aa"),  is(false));
+		assertThat(q.isUnique("ab"),  is(true));
+		assertThat(q.isUnique("abc"), is(true));
+		assertThat(q.isUnique("abca"), is(false));
+		assertThat(q.isUnique("ああ"), is(false));
+	}
+	
+	@Test
+	public void testIsUniceChars() {
+		Q01 q = new Q01();
+		assertThat(q.isUniqueChars("a"),   is(true));
+		assertThat(q.isUniqueChars("aa"),  is(false));
+		assertThat(q.isUniqueChars("ab"),  is(true));
+		assertThat(q.isUniqueChars("abc"), is(true));
+		assertThat(q.isUniqueChars("abca"), is(false));
+	}
+	
+	@Test
+	public void testIsUniceChars2() {
+		Q01 q = new Q01();
+		assertThat(q.isUniqueChars2("a"),   is(true));
+		assertThat(q.isUniqueChars2("aa"),  is(false));
+		assertThat(q.isUniqueChars2("ab"),  is(true));
+		assertThat(q.isUniqueChars2("abc"), is(true));
+		assertThat(q.isUniqueChars2("abca"), is(false));
 	}
 
 	@Test(timeout=1000)
-	public void testIsUnice負荷テスト() {
+	public void testIsUnique負荷テスト() {
 		for (int i = 0; i < 1000000; i++) {
 			Q01 q = new Q01();
-			boolean actual = q.isUnice("アダムスbcde");
+			boolean actual = q.isUnique("アダムスbcde");
+			assertThat(actual, is(true));
+		}
+	}
+	
+	@Test(timeout=1000)
+	public void testIsUniqueChar負荷テスト() {
+		for (int i = 0; i < 1000000; i++) {
+			Q01 q = new Q01();
+			boolean actual = q.isUniqueChars("abcdefghijk");
+			assertThat(actual, is(true));
+		}
+	}
+	
+	@Test(timeout=1000)
+	public void testIsUniqueChars2負荷テスト() {
+		for (int i = 0; i < 1000000; i++) {
+			Q01 q = new Q01();
+			boolean actual = q.isUniqueChars2("abcdefghijk");
 			assertThat(actual, is(true));
 		}
 	}
